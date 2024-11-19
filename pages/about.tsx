ca431bc89b94da57f0d6470c7732daa3642766c7
nick harrison <87631315+Shinyylel@users.noreply.github.com>
2024-11-19 11:34:24 -0600

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -11,11 +11,9 @@ export default function About(){
                         <Image className="about-pic" src="/IMG_0240.JPG" layout='fill'/>
                     </div>
                     <div className="info-container">
-                        <p>Harrison Printing and Promotions has been your trusted partner for high-quality promotional products, screen printing, paper goods, and embroidery since 2007. Founded by William Harrison, we have built a reputation for exceptional craftsmanship and personalized service.
-                        <br>    
-                        From eye-catching custom apparel to branded promotional items, we’re here to help you bring your ideas to life and make a lasting impression. Whether you’re looking for uniforms, event swag, or business essentials, we ensure every product reflects your brand’s identity with precision and style.
-                            <br>
-                        At Harrison Printing and Promotions, we’re more than just a printing company—we’re your partner in making your vision a reality. Let’s create something amazing together!</p>
+                        <p>Harrison Printing and Promotions has been your trusted partner for high-quality promotional products, screen printing, paper goods, and embroidery since 2007. Founded by William Harrison, we have built a reputation for exceptional craftsmanship and personalized service.</p>
+                        <p>From eye-catching custom apparel to branded promotional items, we’re here to help you bring your ideas to life and make a lasting impression. Whether you’re looking for uniforms, event swag, or business essentials, we ensure every product reflects your brand’s identity with precision and style.</p>
+                        <p>At Harrison Printing and Promotions, we’re more than just a printing company—we’re your partner in making your vision a reality. Let’s create something amazing together!</p>
                     </div>
                 </div>
             </div>
