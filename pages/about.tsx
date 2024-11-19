45197ebb109955e9d27ae4f28b8ba109d532c6d1
nick harrison <87631315+Shinyylel@users.noreply.github.com>
2024-11-19 11:47:38 -0600

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -12,7 +12,6 @@ export default function About(){
                     <div className="info-container">
                             <h1 className="aboutContainer">About us</h1>
                        <div className="info-container">
-                            <h1 className="aboutContainer">About us</h1>
                             <p>Harrison Printing and Promotions has been your trusted partner for high-quality promotional products, screen printing, paper goods, and embroidery since 2007. Founded by William Harrison, we have built a reputation for exceptional craftsmanship and personalized service.</p>
                             <br />
                             <p>From eye-catching custom apparel to branded promotional items, we’re here to help you bring your ideas to life and make a lasting impression. Whether you’re looking for uniforms, event swag, or business essentials, we ensure every product reflects your brand’s identity with precision and style.</p>
